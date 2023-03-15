b07a063c074e2ca5064d2cbd772a513c54f20f61
PatrickMykoda <p.mykoda@uniandes.edu.co>
2023-03-15 03:52:47 -0500

    Update

---
+++ app/src/main/java/com/example/amiritmo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.amiritmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlarmasRutina();
+            }
+        });
+
+        ImageButton alarmas = (ImageButton) findViewById(R.id.buttonAlarmas);
+        alarmas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { openListaAlarmas();}
+        });
+    }
+
+    public void openAlarmasRutina() {
+        Intent intent = new Intent(this, AlarmsOfRoutineList.class);
+            startActivity(intent);
+    }
+
+    public void openListaAlarmas() {
+        Intent intent = new Intent(this, AlarmsList.class);
+        startActivity(intent);
+    }
+}
