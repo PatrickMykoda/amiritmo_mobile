d04a0f715a26fa933403e3a7e8df0297d872b082
PatrickMykoda <p.mykoda@uniandes.edu.co>
2023-03-15 11:34:23 -0500

    pm and am buttons added

--- app/src/main/java/com/example/amiritmo/ConfigAlarm.java
+++ app/src/main/java/com/example/amiritmo/ConfigAlarm.java
@@ -21,5 +21,45 @@ public class ConfigAlarm extends AppCompatActivity {
 
             }
         });
+
+        Button am = (Button) findViewById(R.id.am);
+        am.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button am_hide = (Button) findViewById(R.id.am_hide);
+                am_hide.setVisibility(View.VISIBLE);
+                Button pm_hide = (Button) findViewById(R.id.pm_hide);
+                pm_hide.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button pm = (Button) findViewById(R.id.pm);
+        pm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button am_hide = (Button) findViewById(R.id.am_hide);
+                am_hide.setVisibility(View.VISIBLE);
+                Button pm_hide = (Button) findViewById(R.id.pm_hide);
+                pm_hide.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button pmHide = (Button) findViewById(R.id.pm_hide);
+        pmHide.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button am_hide = (Button) findViewById(R.id.am_hide);
+                am_hide.setVisibility(View.INVISIBLE);
+                Button pm_hide = (Button) findViewById(R.id.pm_hide);
+                pm_hide.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        Button amHide = (Button) findViewById(R.id.am_hide);
+        amHide.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button am_hide = (Button) findViewById(R.id.am_hide);
+                am_hide.setVisibility(View.INVISIBLE);
+                Button pm_hide = (Button) findViewById(R.id.pm_hide);
+                pm_hide.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
